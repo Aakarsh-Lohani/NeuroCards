b4ab205f2a0c33c3ace2dce3c2994130d8edfd02
AAKARSH LOHANI <aakarshlohani01@gmail.com>
2025-08-14 11:02:37 +0530

    bug fixed

--- app/documentation/page.js
+++ app/documentation/page.js
@@ -1,6 +1,5 @@
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
-import { motion } from 'framer-motion';
 
 const TechStack = [
     { name: 'Next.js', desc: 'React framework for production' },
